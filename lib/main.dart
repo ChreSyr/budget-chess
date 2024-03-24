ebd222370e57ab126bacfae798a68b5cea2fa22c
Syméon ROUGEVIN <symeon@oclocher.fr>
2024-03-24 16:52:57 +0100

    Changed icon & added native splash

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:crea_chess/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -20,6 +22,7 @@ Future<void> main() async {
   );
   
   // await FirebaseAppCheck.instance.activate();
-
+  FlutterNativeSplash.remove();
+  
   runApp(const BudgetChess());
 }
